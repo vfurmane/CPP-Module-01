87017fbe6681147d93d87bc8ae42102871f4ea2d
vfurmane <vfurmane@student.42.fr>
2021-11-26 12:04:56 +0100

    feat [ex04]: check files open errors

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 12:50:42 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/11/14 13:54:37 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/11/26 12:03:49 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,13 @@
 #include <iostream>
 #include <fstream>
 
-int	main(int argc, char **argv)
+void	read_and_replace(std::ifstream &ifs, std::ofstream &ofs,
+		std::string &to_find, std::string &replace_by)
 {
 	size_t		pos;
-	std::string	to_find(argv[2]);
-	std::string	replace_by(argv[3]);
 	char		buffer[1024];
 	std::string	line;
 
-	if (argc != 4)
-	{
-		std::cout << "ERROR" << std::endl;
-		return (1);
-	}
-	std::ifstream ifs(argv[1]);
-	std::ofstream ofs((std::string(argv[1]) + ".replace").c_str());
 	while (ifs.getline(buffer, 1024))
 	{
 		pos = 0;
@@ -41,6 +33,31 @@ int	main(int argc, char **argv)
 		}
 		ofs << line << std::endl;
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	std::string	to_find(argv[2]);
+	std::string	replace_by(argv[3]);
+
+	if (argc != 4)
+	{
+		std::cout << "ERROR" << std::endl;
+		return (1);
+	}
+	std::ifstream ifs(argv[1]);
+	if (!ifs.good())
+	{
+		std::cout << "ERROR" << std::endl;
+		return (1);
+	}
+	std::ofstream ofs((std::string(argv[1]) + ".replace").c_str());
+	if (!ofs.good())
+	{
+		std::cout << "ERROR" << std::endl;
+		return (1);
+	}
+	read_and_replace(ifs, ofs, to_find, replace_by);
 	ifs.close();
 	ofs.close();
 	return (0);
