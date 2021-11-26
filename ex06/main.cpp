16ac9147cd93dea0bd40a66b31c25810b857afbb
vfurmane <vfurmane@student.42.fr>
2021-11-26 14:13:38 +0100

    fix [ex06]: use fallthrough attribute in switch case

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 14:00:57 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/11/26 13:06:54 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/11/26 14:13:04 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,42 +27,33 @@ int	main(int argc, char **argv)
 		std::cout << std::endl << std::endl;
 		return (1);
 	}
-	std::string	level = argv[1];
-	int	end_of_complains = 0;
-	while (!end_of_complains)
+	switch (smith.getComplainIndex(argv[1]))
 	{
-		switch (smith.getComplainIndex(level))
-		{
-			case DEBUG_INDEX:
-				std::cout << "[ DEBUG ]" << std::endl;
-				smith.complain("DEBUG");
-				std::cout << std::endl;
-				level = "INFO";
-				break ;
-			case INFO_INDEX:
-				std::cout << "[ INFO ]" << std::endl;
-				smith.complain("INFO");
-				std::cout << std::endl;
-				level = "WARNING";
-				break ;
-			case WARNING_INDEX:
-				std::cout << "[ WARNING ]" << std::endl;
-				smith.complain("WARNING");
-				std::cout << std::endl;
-				level = "ERROR";
-				break ;
-			case ERROR_INDEX:
-				std::cout << "[ ERROR ]" << std::endl;
-				smith.complain("ERROR");
-				std::cout << std::endl;
-				end_of_complains = 1;
-				break ;
-			default:
-				std::cout << "[ Probably complaining about insignificant problems ]";
-				std::cout << std::endl;
-				return (1);
-				break ;
-		}
+		case DEBUG_INDEX:
+			std::cout << "[ DEBUG ]" << std::endl;
+			smith.complain("DEBUG");
+			std::cout << std::endl;
+			__attribute__ ((fallthrough));
+		case INFO_INDEX:
+			std::cout << "[ INFO ]" << std::endl;
+			smith.complain("INFO");
+			std::cout << std::endl;
+			__attribute__ ((fallthrough));
+		case WARNING_INDEX:
+			std::cout << "[ WARNING ]" << std::endl;
+			smith.complain("WARNING");
+			std::cout << std::endl;
+			__attribute__ ((fallthrough));
+		case ERROR_INDEX:
+			std::cout << "[ ERROR ]" << std::endl;
+			smith.complain("ERROR");
+			std::cout << std::endl;
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]";
+			std::cout << std::endl << std::endl;
+			return (1);
+			break ;
 	}
 	return (0);
 }
